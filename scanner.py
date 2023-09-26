af3e2d21f6669e41144bb56ef7787340b518b6ab
rahulmalakar42 <malakarahul111@gmail.com>
2023-09-26 21:36:37 +0530

    file changes and code updates

--- scanner.py
+++ scanner.py
@@ -1,6 +1,9 @@
 import cv2
+import os
 
-image = cv2.imread('docscanner python/cam.jpg') #cam2 tu second testing image
+imgpath = os.getcwd() + "\eVault\doc_scanner\captures\cam2.jpg"
+
+image = cv2.imread(imgpath)
 
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -14,13 +17,19 @@ if contours:
 
     document_region = image[y:y+h, x:x+w]
 
-    alpha = 1.3 
+    alpha = 1.2
     beta = 2 
     enhanced_document_region = cv2.convertScaleAbs(document_region, alpha=alpha, beta=beta)
 
     cv2.imwrite('cropped_document_contrast_enhanced.jpg', enhanced_document_region)
 
+    cv2.namedWindow('Cropped Document with Contrast Enhancement', cv2.WINDOW_NORMAL)
     cv2.imshow('Cropped Document with Contrast Enhancement', enhanced_document_region)
+    
+    cv2.namedWindow('Original Document', cv2.WINDOW_NORMAL) 
+    cv2.resizeWindow('Original Document', 450, 750)
+    cv2.imshow('Original Document', image)
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 else:
